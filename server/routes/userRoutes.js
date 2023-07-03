f3ec46d22aa91d7a3d62af2619ce3cd3da61a3dd
Syed Mohd Huzam <mohdhuzam002@gmail.com>
2023-07-03 08:59:23 +0530

    otp service has been implemented with grace of ALLAH

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const account = require('../models/userAccountsModel')
 const path = require('path')
 const alert = require('alert')
-
+const { TWILIO_SERVICE_SID, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env
+const client = require('twilio')(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,{lazyLoading:true})
 
 router.route('/login').get((req,res)=>{
     const room_id = req.query.room_id
@@ -26,6 +27,8 @@ router.route('/checkUser').post(async (req, res) => {
             phone : phone,
             condition :true  
         }
+        const otpResponse= await client.verify.services(TWILIO_SERVICE_SID).verifications.create({ to : `+91${phone}`,channel:"sms"});
+        console.log(JSON.stringify(otpResponse)) 
         res.render('signup',{data})
     }    
   
@@ -39,24 +42,31 @@ const fast2sms = require('fast-two-sms')
 router.route('/add').post(async (req,res)=>{
     const phone = req.body.phone 
     console.log(phone)
-    var options = {
-                
-    } 
+
     const password = req.body.password
     const enteredOtp = req.body.otp
-    let sentOtp = "1234"
-    
-    if( enteredOtp === sentOtp)
-    {
+
         const newUser = new account({_id:phone,password:password})
         await newUser.save()
         const user = { phone : phone , loggedIn: "true"}
         req.session.user = user
-        res.redirect('/')
-    }
-    else 
-    {
-        alert(" wrong otp ")
+        res.send("success")
+
+
+})
+router.route('/verifyOtp').post(async (req,res)=>{
+    const enteredOtp = req.body.enteredOtp ; 
+    const phone  = req.body.phone ; 
+    console.log(enteredOtp,phone)
+    try{
+        const verifiedResponse = await client.verify.services(TWILIO_SERVICE_SID).verificationChecks.create({
+            to: `+91${phone}`,
+            code : enteredOtp
+        })  
+        res.send(`${JSON.stringify(verifiedResponse)}`)
+    }   
+    catch(error){
+        res.status(error?.status || 400).send(error?.message || 'something went wrong!');
     }
 })
 router.route('/login').post(async (req,res)=>{
